237bd3eac3723187dc76c2d567eabbc7ffde4b8d
Giovanni Patruno <giovanni.patruno@kwantis.com>
2022-01-02 17:35:20 +0100

    adjust slider

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -1,15 +1,15 @@
 var sliders = Array.from(document.getElementsByClassName("slideshow-container"));
-console.log(sliders);
 var sliderCounters = new Object();
 sliders.forEach(slider => {
-  sliderCounters[slider.id] = 1
-  showSlides(1, slider.id);
+  sliderCounters[slider.id] = 0
+  showSlides(slider.id);
 })
 
 
-function plusSlides(step, sliderId) {
-  sliderCounters[sliderId] += step;
-  
+function plusSlides(step, sliderId, imgCount) {
+  sliderCounters[sliderId] = (sliderCounters[sliderId] + step < 0) ? imgCount - 1 : (sliderCounters[sliderId] + step) % imgCount;
+  showSlides(sliderId);
+}
 
 function currentSlide(sliderId) {
   showSlides(sliderId);
@@ -20,15 +20,15 @@ function showSlides(sliderId) {
   var slideIndex = sliderCounters[sliderId];
   var slides = document.getElementById(sliderId).getElementsByClassName("mySlides");
   var dots = document.getElementById(sliderId).getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}    
+/*   if (n > slides.length) {slideIndex = 1}    
   if (n < 1) {slideIndex = slides.length}
-  console.error(slideIndex);
+  console.error(slideIndex); */
   for (i = 0; i < slides.length; i++) {
       slides[i].style.display = "none";  
   }
   for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
+    dots[i].className = dots[i].className.replace(" active", "");
   }
-  slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
+  slides[slideIndex].style.display = "block";  
+  dots[slideIndex].className += " active";
 }
